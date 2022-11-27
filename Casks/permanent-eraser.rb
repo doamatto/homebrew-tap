bf4122e07c65cd559f698b9565526a2bfc393c71
Matt Ronchetto <hello@doamatto.xyz>
2022-11-26 20:56:11 -0800

    Create permanent-eraser.rb

---
+++ Casks/permanent-eraser.rb
@@ -0,0 +1,11 @@
+cask "permanent-eraser" do
+  version "2.9.1"
+  sha256 "064893df5590f04be441be4e66d4defac011d2f31b48583b8b9ed583a072f120"
+
+  url "http://www.edenwaith.com/downloads/permanenteraser.dmg"
+  name "Permanent Eraser"
+  desc "Secure file erasing utility for Mac OS X "
+  homepage "https://github.com/edenwaith/Permanent-Eraser"
+
+  app "Permanent Eraser.app"
+end
